a93b7c1af7e2a653c9d5faae3f5f7d924c3dce1d
Nikita <tyumencev2004@bk.ru>
2017-03-04 15:10:52 +0300

    Added info about class, updated info about obf

---
+++ ProductInfo.java
@@ -0,0 +1,18 @@
+package com.neonObf;
+
+/**
+ * This class meeded for GIO. Do not delete.
+ */
+public class ProductInfo {
+	public static String getTitle() {
+		return "NeonObf";
+	}
+
+	public static String getVersion() {
+		return "1.2.0";
+	}
+
+	public static String getBuildId() {
+		return "1040";
+	}
+}

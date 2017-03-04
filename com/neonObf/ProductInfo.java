4440bc191649bbed931fa040fe4f1843842717ca
Nikita <tyumencev2004@bk.ru>
2017-03-04 15:12:15 +0300

    Forgot to move to dir before uploading file ;D (#1)
    * Delete ProductInfo.java
    
    * Add files via upload

--- com/neonObf/ProductInfo.java
+++ com/neonObf/ProductInfo.java
@@ -1,15 +1,18 @@
 package com.neonObf;
 
+/**
+ * This class meeded for GIO. Do not delete.
+ */
 public class ProductInfo {
 	public static String getTitle() {
 		return "NeonObf";
 	}
 
 	public static String getVersion() {
-		return "1.0.5";
+		return "1.2.0";
 	}
 
 	public static String getBuildId() {
-		return "1021";
+		return "1040";
 	}
 }
